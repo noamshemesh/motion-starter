9f97c266e7614b29be87db3608ee1fb8117799bd
Noam Shemesh <noamshemesh@gmail.com>
2015-10-25 20:47:17 +0200

    add support for a couple

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ function sendNotOk(res) {
 }
 
 var SERVICE_PATH = 'sudo /usr/sbin/service motion ';
+var map = [];
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function start(key) {
+  if (!isNumeric(key)) {
+    return null;
+  }
+  console.log('Setting ' + key + ' to true. map is ', map);
+  map[key] = true;
+  if (map[0] && map[1]) {
+    return SERVICE_PATH + 'start';
+  }
+
+  return null;
+}
+
+function stop(key) {
+  if (!isNumeric(key)) {
+    return null;
+  }
+
+  console.log('Setting ' + key + ' to false. map is ', map);
+  map[key] = false;
+  return SERVICE_PATH + 'stop';
+}
 
 http.createServer(function (req, res) {
   if (!req.url) return;
@@ -19,34 +47,36 @@ http.createServer(function (req, res) {
   console.log('Received ' + req.url);
 
   if (req.url.indexOf('start') >= 0) {
-    command = SERVICE_PATH + 'start';
+    command = start(req.url[req.url.length - 1] - 1);
   } else if (req.url.indexOf('stop') >= 0) {
-    command = SERVICE_PATH + 'stop';
-  } else if (req.url.indexOf('reboot') >= 0) {
-    command = 'sudo reboot';
+    command = stop(req.url[req.url.length - 1] - 1);
   } else {
     console.log('Unknown command');
     sendNotOk(res);
     return;
   }
 
-  console.log('Executing ' + command);
-  var cmd = process.exec(command);
+  if (command) {
+    console.log('Executing ' + command);
+    var cmd = process.exec(command);
 
-  cmd.stdout.on('data', function(output){
-    console.log(output);
-  });
+    cmd.stdout.on('data', function(output){
+      console.log(output);
+    });
 
-  cmd.on('close', function(){
-    console.log('Finished');
+    cmd.on('close', function(){
+      console.log('Finished');
+      sendOk(res);
+    });
+
+    cmd.stderr.on('data', function(err){
+      sendNotOk(res);
+      console.log('Error, printing: ')
+      console.log(err);
+    });
+  } else {
     sendOk(res);
-  });
-
-  cmd.stderr.on('data', function(err){
-    sendNotOk(res);
-    console.log('Error, printing: ')
-    console.log(err);
-  });
+  }
 }).listen(8125);
 
 console.log('Server running on port 8125');
