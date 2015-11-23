1d3a4dcad61518cfcb8fbb04c4c35cb5353b616f
Noam Shemesh <noam@bigpanda.io>
2015-11-23 23:31:07 +0200

    add milight support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var process = require('child_process');
 var http = require('http');
+var Milight = require('node-milight-promise').MilightController;
+var Commands = require('node-milight-promise').commands;
 
-function sendOk(res) { 
+function sendOk(res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Success');
 }
@@ -48,8 +50,10 @@ http.createServer(function (req, res) {
 
   if (req.url.indexOf('start') >= 0) {
     command = start(req.url[req.url.length - 1] - 1);
+    turnOffMilight();
   } else if (req.url.indexOf('stop') >= 0) {
     command = stop(req.url[req.url.length - 1] - 1);
+    turnOnMilight();
   } else {
     console.log('Unknown command');
     sendNotOk(res);
@@ -80,3 +84,40 @@ http.createServer(function (req, res) {
 }).listen(8125);
 
 console.log('Server running on port 8125');
+
+var milight = new Milight({
+        ip: "192.168.1.109",
+        delayBetweenCommands: 35,
+        commandRepeat: 3
+    });
+
+function isDayLight() {
+  var date = new Date();
+  var hourNow = date.getHours();
+  return hourNow >= 8 && hourNow < 17;
+}
+
+function turnOnMilight() {
+  if (!isDayLight()) {
+    turnGroup(1, true);
+    turnGroup(2, true);
+  }
+}
+
+function turnOffMilight() {
+  turnGroup(1, false);
+  if (!isDayLight()) {
+    turnGroup(2, true);
+  }
+}
+
+function turnGroup(group, state) {
+  if (state) {
+    milight.sendCommands(Commands.rgbw.on(group), Commands.rgbw.whiteMode(group), Commands.rgbw.brightness(20));
+    for (var i = 3; i <= 10; i++) {
+      milight.sendCommands(Commands.rgbw.brightness(10 * i));
+    }
+  }
+
+  milight.sendCommands(Commands.rgbw.off(group));
+}
